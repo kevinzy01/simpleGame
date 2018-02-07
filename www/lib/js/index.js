454b17cb268a58c8027b1f58d1ea84ca994fd936
kevinzy01 <32551468+kevinzy01@users.noreply.github.com>
2018-02-07 12:08:18 +0100

    Update

--- www/lib/js/index.js
+++ www/lib/js/index.js
@@ -4,6 +4,9 @@ var player;
 var cursor;
 var stars;
 var crate;
+var diamonds;
+var diaCount = 0;
+var totalDia = 0;
 var starCount = 0;
 var totalStars = 0;
 var scoreText;
@@ -49,6 +52,24 @@ function create3Stars(x, y) {
   }
 }
 
+function create2Stars(x, y) {
+  for (var i = 250; i < 330; i+= 40) {
+    //create a star inside star group
+    var star = stars.create(i + x, y, "star")
+    //make stars static
+    star.body.gravity.y = 0;
+    totalStars++;
+  }
+}
+
+function createDia(x, y) {
+    var diamond = diamonds.create(x, y, "diamond");
+    //add 1 to diamonds count
+    totalDia += 1;
+    //make diamond bigger
+    diamond.scale.setTo(1.25, 1.25)
+};
+
 function createPlatform(x, y, leftTile, middleTile, rightTile) {
   var plat = platforms.create(x, y, middleTile);
   plat.body.immovable = true;
@@ -61,9 +82,9 @@ function createPlatform(x, y, leftTile, middleTile, rightTile) {
   plat.scale.setTo(.5, .5);
 }
 
-function collectStar (player, star) {
+function collectStar (player, stars) {
   //remove star
-  star.kill()
+  stars.kill()
   //add 10 points to score
   starCount += 1;
   scoreText.text = "Stars: " + starCount + " / " + totalStars;
@@ -71,10 +92,18 @@ function collectStar (player, star) {
   starSound.play()
 };
 
+function collectDia (player, diamonds) {
+  //remove star
+  diamonds.kill()
+  //add 10 points to score
+  diaCount += 1;
+  //add diamond sound
+};
+
 //first game state
 var GameState = {
   preload: function () {
-    this.load.image("fullTree", "assets/bg/png/Object/Tree_2.png")
+    this.load.spritesheet("fullTree", "assets/bg/png/Object/Tree_2.png")
     this.load.image("bg", "assets/bg/png/BG/BG.png")
     this.load.image("leftTile", "assets/bg/png/Tiles/1.png")
     this.load.image("middleTile", "assets/bg/png/Tiles/2.png")
@@ -83,6 +112,7 @@ var GameState = {
     this.load.image("middleFloat", "assets/bg/png/Tiles/14.png")
     this.load.image("rightFloat", "assets/bg/png/Tiles/15.png")
     this.load.image("star", "assets/star.png")
+    this.load.image("diamond", "assets/diamond.png")
     this.load.image("stone", "assets/bg/png/Object/Stone.png")
     this.load.spritesheet("crate", "assets/bg/png/Object/Crate.png")
     this.load.image("tree", "assets/bg/png/Object/Tree_1.png")
@@ -119,10 +149,11 @@ var GameState = {
     createMaximizedItem(258, 610, "stone");
 
     //Creating floating tiles
-    createPlatform(197, 350, "leftFloat", "middleFloat", "rightFloat")
     createPlatform(322, 550, "leftFloat", "middleFloat", "rightFloat")
     createPlatform(547, 450, "leftFloat", "middleFloat", "rightFloat" )
-    createPlatform(500, 185, "leftFloat", "middleFloat", "rightFloat" )
+    createPlatform(197, 350, "leftFloat", "middleFloat", "rightFloat")
+    createPlatform(500, 200, "leftFloat", "middleFloat", "rightFloat" )
+    createPlatform(800, 250, "leftFloat", "middleFloat", "rightFloat" )
 
     //add the character
     player = game.add.sprite(32, game.world.height - 150, "dude");
@@ -139,15 +170,22 @@ var GameState = {
     player.animations.add("left", [0, 1, 2, 3], 10, true);
     player.animations.add("right", [5, 6, 7, 8], 10, true);
 
+
     //add controls
     cursors = this.input.keyboard.createCursorKeys();
 
-    //adding stars
+    //adding diamond
+    diamonds = this.add.group();
+    diamonds.enableBody = true;
+    createDia(816, 210)
+
+    //adding stars //substract 272 from central platform x
     stars = this.add.group();
     stars.enableBody = true;
-
+    create3Stars(50, 510);
     create3Stars(275, 410);
-    create3Stars(55, 510);
+    create3Stars(-75, 310);
+    create3Stars(228, 160);
 
     //add a score
     scoreText = game.add.text(16, 16, "Stars: 0 / " + totalStars , {fontSize: "32px", fill: "#000"})
@@ -160,13 +198,15 @@ var GameState = {
     //check if player overlaps stars
     game.physics.arcade.overlap(player, stars, collectStar, null, this);
 
+    //check if player overlaps diamond
+    game.physics.arcade.overlap(player, diamonds, collectDia, null, this);
+
     //collide crates with world and player
     game.physics.arcade.collide(crates, platforms);
 
     //make crate move if touched by player
     var playerHitCrate = game.physics.arcade.collide(crates, player);
 
-    //make crate slow
     // game.physics.arcade.acceleration(crates, 20)
 
     //make player move
